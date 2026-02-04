9223314be3668ff2e9d9e33559b1b5414c965853
lightchainui <lightchainui@gmail.com>
2026-02-04 16:26:39 +0600

    removed unused components

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -4,7 +4,3 @@ export { ProductShowcase } from "./ProductShowcase";
 export { FoundersMoat } from "./FoundersMoat";
 export { SolutionsSection } from "./SolutionsSection";
 export { AnswerNuggets } from "./AnswerNuggets";
-// Legacy exports (kept for backwards compatibility)
-export { ProductGrid } from "./ProductGrid";
-export { FomoGenFeatured } from "./FomoGenFeatured";
-export { TechnicalFAQ } from "./TechnicalFAQ";
