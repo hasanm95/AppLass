f6855051932fe206ffeaeb544d056e85a01cfda9
hasanm95 <hsncool10@gmail.com>
2026-02-21 16:56:11 +0600

    sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://applass.com",
+  integrations: [react(), sitemap()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
