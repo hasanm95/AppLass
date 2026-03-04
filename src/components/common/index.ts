91dfe3d1d878c9e5bc30b013fe5a0def91c23258
hasanm95 <hsncool10@gmail.com>
2026-03-04 11:06:39 +0600

    fix: build issues

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,1 @@
-
-export { Footer } from "./Footer";
 export { FAQSchema } from "./FAQSchema";
-export { FAQRegistrySection } from "./FAQRegistrySection";
-export { SubpageHero } from "./SubpageHero";
