c75fbe3a62afeb1233e503e1daa2ee337d113c63
hasanm95 <hsncool10@gmail.com>
2026-03-04 11:36:47 +0600

    fix: home page components

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,6 +1,1 @@
-export { Hero } from "./Hero";
-export { SocialProof } from "./SocialProof";
-export { ProductShowcase } from "./ProductShowcase";
-export { FoundersMoat } from "./FoundersMoat";
-export { SolutionsSection } from "./SolutionsSection";
 export { AnswerNuggets } from "./AnswerNuggets";
